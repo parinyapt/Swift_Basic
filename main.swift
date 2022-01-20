965918582111b6f317ee5637d83fea69ca4a01ba
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2022-01-20 11:13:26 +0000

    final

--- main.swift
+++ main.swift
@@ -1,1 +1,46 @@
-print("Hello World! commit git test")
+import Foundation
+
+print("Hello Prin")
+print(1+5)
+
+var a = 5
+var b=4
+var a1234 = "สวัสดี"
+var ทดสอบ = "Hello Parinya"
+
+print("ค่าที่แสดงในตัวแปร ",a1234)
+
+var x1:Int
+//Integer จำนวนเต็ม 
+//จำนวนบวก Positive Integer
+//จำนวนลบ Negative Integer
+
+x1 = 78999999
+x1 = -99999
+
+//Real NUmber จำนวนจริง
+//ex. 10.12
+var pi:Double = 3.14158
+print(pi)
+
+//Boolean
+var status:Bool
+status = true
+print(status)
+
+//String
+var str:String
+str = "1" + "2" + "World"
+print(str)
+
+var n:Int
+n = 1+2+3+4
+print("value n =",n)
+
+let n1:Int
+n1 = 2
+//n1 = 3 // error
+
+var n2:Int
+n2 = 1
+n2 = 3
